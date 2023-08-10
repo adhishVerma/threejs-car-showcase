535488137e0535ad69343c2baaf7a09a78144d3c
adhishVerma <v.adhish16@gmail.com>
2023-08-10 17:58:47 +0530

    lighting:studio

--- src/World/components/lights.js
+++ src/World/components/lights.js
@@ -9,24 +9,23 @@ import {
 import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
 
 function createLights() {
-  const sceneLight = new HemisphereLight(0x0000ff, '#878787', 1 );
-  sceneLight.position.set(0, 20, 0);
+  const sceneLight = new HemisphereLight(0x0000ff, '#878787', 0 );
 
-  const ambientLight = new AmbientLight('#878787', 3)
+  const ambientLight = new AmbientLight('#878787', 0.7)
 
-  const mainLight = new SpotLight(0xffffff, 1000);
-  mainLight.position.set(5, 25, 7.5);
+  const mainLight = new DirectionalLight("white", 0);
+  mainLight.position.set(10, 10, 10);
 
-  const lightbulb = new DirectionalLight(0xffffff, 1);
+  const lightbulb = new DirectionalLight("#ffffff", 1);
   lightbulb.position.set(3,3,3)
 
-  const lightbulb2 = new DirectionalLight(0xffffff, 1);
+  const lightbulb2 = new DirectionalLight("#ffffff", 1);
   lightbulb2.position.set(-3,3,3);
 
-  const lightbulb3 = new DirectionalLight(0xffffff, 1);
+  const lightbulb3 = new DirectionalLight("#ffffff", 1);
   lightbulb3.position.set(-3,3,-3);
 
-  const lightbulb4 = new DirectionalLight(0xffffff, 1);
+  const lightbulb4 = new DirectionalLight("#ffffff", 1);
   lightbulb4.position.set(3,3,-3);
 
   return { sceneLight, mainLight, ambientLight, lightbulb, lightbulb2, lightbulb3, lightbulb4};
